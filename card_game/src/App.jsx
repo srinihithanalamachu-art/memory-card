a616e194e17bef8d4f3a35f12c812dfa24545788
Srinihitha Nalamachu <srinihitha.nalamachu@creditsafe.com>
2026-03-15 22:39:00 +0530

    flipping the card

--- card_game/src/App.jsx
+++ card_game/src/App.jsx
@@ -1,6 +1,6 @@
 import GameHeader from "./components/GameHeader";
 import Card from "./components/Card";
-
+import {useState,useEffect} from "react";
 const cardValues=[
   "🍎",
   "🍌",
@@ -22,14 +22,55 @@ const cardValues=[
 
 ]
 function App() {
- 
+ const [cards,setCards]=useState([]);
+ const initializeGame=()=>
+ {
+  //SHUFFLE THE CARDS
+
+    const finalCards=cardValues.map((value,index)=>
+    (
+      {
+        id:index,
+        value,
+        isFlipped:false,
+        isMatched:false,
+      }
+    ));
+    setCards(finalCards);
+ };
+ //when the component renders , the function need to be called
+ useEffect(()=>
+{
+  initializeGame();
+},[]);
+
+  const handleCardClick=(card)=>
+  {
+    //don't allow clicking if card is already clicked
+    if(card.isFlipped || card.isMatched)
+    {
+      return;
+    }
+    //update card flipped state
+    const newCards=cards.map((c)=>
+    {
+      if(c.id===card.id)
+      {
+        return { ...c,isFlipped:true};
+      }
+      else{
+        return c;
+      }
+    });
+    setCards(newCards);
+  };
 
   return (
     <div className="App">
     <GameHeader score={3} moves={10}/>
     <div className="cards-grid">
-       {cardValues.map((card)=>(
-        <Card card={card} />
+       {cards.map((card)=>(
+        <Card card={card} onClick={handleCardClick} />
       ))}
     </div>
     </div>
--- card_game/src/components/Card.jsx
+++ card_game/src/components/Card.jsx
@@ -1,9 +1,9 @@
-function Card(props)
+function Card({card,onClick})
 {
   return (
-    <div className="card">
+    <div className={`card ${card.isFlipped ? "flipped" : ""}`} onClick={()=>onClick(card)}>
         <div className="card-front"> ?</div>
-      <div className="card-back">{props.card}</div>
+      <div className="card-back">{card.value}</div>
     </div>
   );
 }
